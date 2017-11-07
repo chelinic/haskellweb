2b7bdbcb1c5657d3d9f1b26d3f89967f31d24798
romefeller <algarcia@usp.br>
2017-11-07 23:52:03 +0000

    Front-end pt2

--- HaskellWeb01/src/Handler/Pagina.hs
+++ HaskellWeb01/src/Handler/Pagina.hs
@@ -9,10 +9,9 @@ module Handler.Pagina where
 import Import
 import Text.Julius
 
-
 -- não fechar as tags 
 -- o haskell fecha automaticamente
-
+-- pode fechar tag inline
 
 getPag1R :: Handler Html
 getPag1R = do
@@ -25,7 +24,7 @@ getPag1R = do
         [whamlet|
             <h1> Pagina 1
             <a href=@{HomeR}> Voltar
-        |]
+    |]
 
 getPag2R :: Handler Html
 getPag1R = do
@@ -39,7 +38,6 @@ getPag1R = do
             <h1> Pagina 1
             <a href=@{HomeR}> Voltar
         |]
-
 
 getPag3R :: Handler Html
 getPag1R = do
@@ -62,9 +60,6 @@ getHomeR = do
         addStylesheet $ (StaticR css_bootstrap_css)
         $ (whamletFile "templates/home.whamlet")
         
-        
--- pode fechar tag inline
-
 soma :: Int -> Int -> Int
 soma x y = x + y
 
@@ -75,3 +70,57 @@ getAddR x y do
         <h1> A soma deu: #{soma x y}
     |]
 
+
+paginaDentro :: Widget
+paginaDentro = do
+    toWidget $ [cassius|
+        h1
+            color:orange;
+    |]
+    [whamlet|
+        <h1>Ola Mundo
+    |]
+    
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (x:[]) = Nothing
+safeTail (_:xs) = Just xs
+
+getTailR :: Text -> Handler Html
+getTailR palavra = do
+    palstring <- return $ unpack palavra
+    t <- return $ safeTail palstring -- Handler (Maybe [a])
+    defaultLayout $ do
+        [whamlet|
+            $maybe jt <-t
+                <h1> O Tail eh: #{jt}
+            $Nothing
+                <h1> Deu ruim aqui 
+        |]
+
+-- unpack retorna string 
+-- return para colocar algo puro dentro da mônada
+-- $ para monada Maybe
+
+
+-- lista de itens
+getExemploR :: Handler Html
+getExemploR = do
+    defaultLayout $ do
+    addStylesheet $ (StaticR css_home_css)
+    [whamlet|
+        <div class="container">
+            ^{paginaDentro}
+    |]
+    
+getListR :: Handler Html
+getListR = do
+    lista <- return $ ["Santos","Gremio","Palmeiras","Cruzeiro","Botafogo","Flamengo"] :: Handler [String]
+    defaultLayout $ do
+    [whamlet|
+        <ul>
+            $forall time <- lista
+                <li> #{pack time}
+    |]
+    
+    
